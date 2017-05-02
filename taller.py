f58485c03d10062c3febf41981cbdb595964f667
jupazulu <juanpablo99@javerianacali.edu.co>
2017-05-02 11:14:48 -0500

    Initial commit

---
+++ taller.py
@@ -0,0 +1,32 @@
+def prestamo(valor,tasa,cuota):
+    
+    """
+    print("No. de Cuotas \t Pago a capital \t pago de intereses \t cuota del mes \t saldo de la deuda despues del pago")
+    
+    c1=0
+    c2=0
+    c3=0
+    c4=0
+    z= valor
+    w=1
+    y=0
+    while(cuota>c1 and z>c2 and w>c1):
+        c1=c1+1
+        z=int(valor/cuota)
+        c2=c2+1
+        w=(valor*tasa/100)
+        print(str(c1))
+    """
+        
+    print("|No. de Cuotas | \t Pago a capital | \t pago de intereses | \t cuota del mes | \t saldo de la deuda despues del pago|")
+    print("=====================================================================================================================")
+    c1=0
+    c2=0
+    c3=0
+
+    while(cuota>c1):
+        c1=c1+1
+    print(c1)
+    
+
+        
